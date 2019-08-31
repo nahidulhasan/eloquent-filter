c9bd78a7dccbe6ca899748dbf33ada3f837afc33
nahid35 <nahidul.cse@gmail.com>
2019-08-31 21:21:56 +0600

    Added files

---
+++ src/EloquentFilterServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace NahidulHasan\EloquentFilter;
+
+use Illuminate\Support\ServiceProvider;
+
+class EloquentFilterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
